087c84a152150c714314e30116d5db25d9519afa
jammealexandre28 <jammealexandre1@gmail.com>
2023-04-07 12:24:01 +0200

    test

---
+++ script.js
@@ -0,0 +1,6 @@
+fetch("http://localhost:3000/year")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    document.querySelector("#year").textContent = data.year;
+  });
